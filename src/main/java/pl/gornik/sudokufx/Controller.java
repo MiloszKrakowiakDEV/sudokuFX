1f1a2496aa84cb1009f419dd92b772179dcfe65e
Endosz <miloszz.krakowiak@gmail.com>
2025-05-19 13:00:05 +0200

    Added pre displayed tiles

--- src/main/java/pl/gornik/sudokufx/Controller.java
+++ src/main/java/pl/gornik/sudokufx/Controller.java
@@ -1,11 +1,16 @@
 package pl.gornik.sudokufx;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
@@ -21,6 +26,8 @@ public class Controller {
     private GridPane sudokuGrid;
     @FXML
     private Label lblNumber;
+    @FXML
+    private ComboBox<String> comboDifficulty;
 
     private int[][] gameNumbersArr = new int[9][9];
 
@@ -30,9 +37,60 @@ public class Controller {
 
     private String chosenColor="#FFF";
 
+    private int displayTiles=40;
+
     public void initialize(){
+        List<String> difficulties = new ArrayList<>();
+        difficulties.add("Łatwy");
+        difficulties.add("Średni");
+        difficulties.add("Trudny");
+        comboDifficulty.valueProperty().addListener(observable -> {
+            switch (comboDifficulty.valueProperty().getValue()){
+                case "Łatwy"->{
+                    displayTiles=40;
+                }
+                case "Średni"->{
+                    displayTiles=32;
+                }
+                case "Trudny"->{
+                    displayTiles=26;
+                }
+            }
+        });
+        comboDifficulty.setItems(FXCollections.observableList(difficulties));
         generateNumbers();
+        for(int[] i : gameNumbersArr){
+            System.out.println(Arrays.toString(i));
+        }
+        sudokuGrid.getChildren().clear();
         generateBoard();
+        displayTips();
+//        fillNumbers();
+    }
+
+    public void displayTips(){
+        MouseEvent mouseEvent1 = new MouseEvent(
+                MouseEvent.MOUSE_PRESSED,
+                0, 0, 0, 0,
+                MouseButton.PRIMARY,
+                1,
+                false, false, false, false,
+                true, false, false, true,
+                false, false, null
+        );
+        Random random = new Random();
+        int index = 0;
+        for (int i = 0; i < displayTiles; i++) {
+            index = random.nextInt(0,81);
+            if(((Label)((StackPane)sudokuGrid.getChildren().get(index)).getChildren().get(0)).getText().equals("")){
+                int row = index / 9;
+                int col = index % 9;
+                selectNumber(gameNumbersArr[row][col]);
+                sudokuGrid.getChildren().get(index).fireEvent(mouseEvent1);
+                sudokuGrid.getChildren().get(index).setOnMousePressed(null);
+                ((Label)((StackPane)sudokuGrid.getChildren().get(index)).getChildren().get(0)).setStyle("-fx-border-color: green;-fx-border-width: 3;");
+            }
+        }
     }
 
     public void generateBoard(){
@@ -67,10 +125,19 @@ public class Controller {
                     ((Label)cell.getChildren().get(0)).setText(String.valueOf(chosenNumber));
                     ((Label)cell.getChildren().get(0)).setBackground(Background.fill(Paint.valueOf(chosenColor)));
                     ((Label)cell.getChildren().get(0)).setTextFill(Paint.valueOf("#FFF"));
+                    userNumbersArr[row][col]=chosenNumber;
+                    if(checkForWin()){
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Koniec");
+                        alert.setHeaderText(null);
+                        alert.setContentText("Wygrałeś!");
+                        alert.showAndWait();
+                    }
                 }else{
                     ((Label)cell.getChildren().get(0)).setText("");
                     ((Label)cell.getChildren().get(0)).setBackground(Background.fill(Paint.valueOf("#FFF")));
                     ((Label)cell.getChildren().get(0)).setTextFill(Paint.valueOf("#000"));
+                    userNumbersArr[row][col]=0;
                 }
             });
         }
@@ -107,17 +174,45 @@ public class Controller {
     private boolean isSafe(int row, int col, int num) {
         // Row and column check
         for (int i = 0; i < 9; i++) {
-            if (gameNumbersArr[row][i] == num || gameNumbersArr[i][col] == num)
+            if ((gameNumbersArr[row][i] == num && i!=col) || (gameNumbersArr[i][col] == num&&i!=row)){
                 return false;
+            }
         }
+
 
         // 3x3 box check
         int boxStartRow = (row / 3) * 3;
         int boxStartCol = (col / 3) * 3;
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
-                if (gameNumbersArr[boxStartRow + i][boxStartCol + j] == num)
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (gameNumbersArr[boxStartRow + i][boxStartCol + j] == num && boxStartRow+i!=row && boxStartCol+j!=col) {
                     return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isSafeUser(int row, int col, int num) {
+        // Row and column check
+        for (int i = 0; i < 9; i++) {
+            if ((userNumbersArr[row][i] == num && i!=col) || (userNumbersArr[i][col] == num&&i!=row)){
+                return false;
+            }
+        }
+
+
+        // 3x3 box check
+        int boxStartRow = (row / 3) * 3;
+        int boxStartCol = (col / 3) * 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (gameNumbersArr[boxStartRow + i][boxStartCol + j] == num && boxStartRow+i!=row && boxStartCol+j!=col) {
+                    return false;
+                }
+            }
+        }
 
         return true;
     }
@@ -144,5 +239,57 @@ public class Controller {
             case "9"-> chosenColor="black";
         }
         lblNumber.setStyle("-fx-border-color: #000;-fx-text-fill: #FFF;-fx-background-color: "+chosenColor);
+    }
+
+    public boolean checkForWin(){
+
+        for (int i = 0; i < 81; i++) {
+            int row = i/9;
+            int col = i%9;
+            if(userNumbersArr[row][col]==0) return false;
+            if(isSafeUser(row,col,userNumbersArr[row][col])) continue;
+            return false;
+        }
+        return true;
+    }
+
+    public void fillNumbers(){
+        MouseEvent mouseEvent1 = new MouseEvent(
+                MouseEvent.MOUSE_PRESSED,
+                0, 0, 0, 0,
+                MouseButton.PRIMARY,
+                1,
+                false, false, false, false,
+                true, false, false, true,
+                false, false, null
+        );
+
+        for (int i = 0; i < 81; i++) {
+            int row = i / 9;
+            int col = i % 9;
+            selectNumber(gameNumbersArr[row][col]);
+            sudokuGrid.getChildren().get(i).fireEvent(mouseEvent1);
+        }
+    }
+
+    public void selectNumber(int number) {
+        chosenNumber=number;
+        lblNumber.setText(String.valueOf(number));
+        switch (String.valueOf(number)){
+            case "1"-> chosenColor="pink";
+            case "2"-> chosenColor="cyan";
+            case "3"-> chosenColor="yellow";
+            case "4"-> chosenColor="red";
+            case "5" -> chosenColor="lime";
+            case "6" -> chosenColor="magenta";
+            case "7" -> chosenColor="brown";
+            case "8"-> chosenColor="gray";
+            case "9"-> chosenColor="black";
+        }
+        lblNumber.setStyle("-fx-border-color: #000;-fx-text-fill: #FFF;-fx-background-color: "+chosenColor);
+    }
+
+    public void restart(){
+        initialize();
     }
 }
